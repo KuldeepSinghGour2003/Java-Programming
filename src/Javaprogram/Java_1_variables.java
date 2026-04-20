195ec9e49747815db10cf6ba5ca2f7a783e701ff
KuldeepSinghGour <kuldeepsinghgour631@gmail.com>
2026-04-20 22:47:17 +0530

    Added First Java Program

---
+++ src/Javaprogram/Java_1_variables.java
@@ -0,0 +1,20 @@
+package Javaprogram;
+
+public class Java_1_variables {
+    public static void main(String[]args)
+    {
+        byte age=99;
+        int age1=98;
+        short age3=89;
+        long agein=8989899999999L;
+        char ch='A';
+        float f=89.979086f;
+        double d=96.99D;
+        boolean b= true;
+        String str="Kuldeep";
+        System.out.println(age);
+        System.out.println(str);
+
+    }
+
+}

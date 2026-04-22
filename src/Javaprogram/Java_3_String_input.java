f8f7d185dc582b2f32f48649df757c1935dcb7b7
KuldeepSinghGour <kuldeepsinghgour631@gmail.com>
2026-04-22 21:39:45 +0530

    Added First Java Program

---
+++ src/Javaprogram/Java_3_String_input.java
@@ -0,0 +1,13 @@
+package Javaprogram;
+import java.util.Scanner;
+
+public class Java_3_String_input {
+    public static void main(String[]args)
+    {
+        System.out.println("Taking input from user:");
+        Scanner sc= new Scanner(System.in);
+        //next Line used to print all string
+        String str=sc.nextLine();
+        System.out.println(str);
+    }
+}

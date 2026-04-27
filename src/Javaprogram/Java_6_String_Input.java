fa423ab408e2bc8f2a996af4755ccda288a3a094
KuldeepSinghGour <kuldeepsinghgour631@gmail.com>
2026-04-27 21:54:01 +0530

    Added First Java Program

---
+++ src/Javaprogram/Java_6_String_Input.java
@@ -0,0 +1,9 @@
+package Javaprogram;
+public class Java_6_String_Input {
+    public static void main(String[]args)
+    {
+       // String name=new String("Kuldeep");
+        String name = "Kuldeep Singh Gour";
+        System.out.println(name);
+    }
+}

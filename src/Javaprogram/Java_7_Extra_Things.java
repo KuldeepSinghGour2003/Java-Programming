2aedbb00863d827da6053b92bcbe53ab826d7a8b
KuldeepSinghGour <kuldeepsinghgour631@gmail.com>
2026-04-27 21:55:18 +0530

    Added First Java Program

---
+++ src/Javaprogram/Java_7_Extra_Things.java
@@ -0,0 +1,20 @@
+package Javaprogram;
+import java.util.Scanner;
+
+public class Java_7_Extra_Things {
+    public static void main(String[] args) {
+        // String name = new String("Harry");
+        // String name = "Harry";
+        // System.out.print("The name is: ");
+        // System.out.print(name);
+        int a = 98;
+        float b = 9.88795f;
+        System.out.printf("The value of a is %d and value of b is %8.2f", a, b);
+        //System.out.format("The value of a is %d and value of b is %f", a, b);
+        Scanner sc = new Scanner(System.in);
+//        String st = sc.next();
+//        String st = sc.nextLine();
+//        System.out.println(st);
+
+    }
+}

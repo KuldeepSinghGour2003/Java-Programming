d3996ffc4997f174907060c4b617ab7a437032a6
KuldeepSinghGour <kuldeepsinghgour631@gmail.com>
2026-06-14 16:31:00 +0530

    Add first Java Program

---
+++ src/Javaprogram/Java_51_JavaDocs.java
@@ -0,0 +1,21 @@
+package Javaprogram;
+
+/**
+ * This class Demonstrate what javadocs is and how it was used in java industry
+ * This is <i>itatic</i>word<p>This is new paragraph</p>
+ * @author Kuldeep (Java world)
+ * @version 9.1
+ * @since 2003
+ * @see <a href="https://download.java.net/java/GA/jdk14/docs/api/index.html">Java docs</a>
+*/
+
+public class Java_51_JavaDocs {
+    public void add(int a,int b)
+    {
+        System.out.println("The sum is:"+ a+b);
+    }
+    public static void main(String[]args)
+    {
+      System.out.println("This is a main class!");
+    }
+}

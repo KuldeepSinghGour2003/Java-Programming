70f7d11a6c6717224438bfb57306e5eb20e2e59b
KuldeepSinghGour <kuldeepsinghgour631@gmail.com>
2026-06-16 21:26:43 +0530

    Add first Java Program

---
+++ src/Javaprogram/PrintTable.java
@@ -0,0 +1,20 @@
+package Javaprogram;
+
+import java.util.Scanner;
+
+public class PrintTable {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num;
+
+        System.out.print("Enter a number: ");
+        num = sc.nextInt();
+
+        for(int i = 1; i <= 10; i++) {
+
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+    }
+}
